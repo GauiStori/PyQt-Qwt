1e8557977d8742c0813c682723b6f8f9e813e7e7
Gudjon I. Gudjonsson <gudjon@gudjon.org>
2016-11-12 08:02:50 +0100

    Add barchart. Not finished.

---
+++ qt5examples/barchart.py
@@ -0,0 +1,149 @@
+#!/usr/bin/python
+
+# python simpleplot.py <qtversion (4 or 5)>
+# Tested for python3, Qt5
+
+import sys
+sys.path.append('../sip/')
+import math, random
+import Qwt
+import numpy as np
+from PyQt5.QtCore import Qt, QTime,  QPointF,  QSize
+from PyQt5.QtGui import QColor,  QTransform
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFrame, QWidget
+from PyQt5.QtGui import QPolygonF
+
+class BarChart( QWidget ):
+    def __init__(self, parent):
+        self.setAutoFillBackground( True )
+        self.setPalette( Qt.white )
+        self.canvas().setPalette( QColor( "LemonChiffon" ) )
+
+        self.setTitle( "Bar Chart" )
+
+        self.setAxisTitle( QwtPlot.yLeft, "Whatever" )
+        self.setAxisTitle( QwtPlot.xBottom, "Whatever" )
+
+        self.d_barChartItem = QwtPlotMultiBarChart( "Bar Chart " )
+        self.d_barChartItem.setLayoutPolicy( QwtPlotMultiBarChart.AutoAdjustSamples )
+        self.d_barChartItem.setSpacing( 20 )
+        self.d_barChartItem.setMargin( 3 )
+
+        self.d_barChartItem.attach( self )
+
+        self.insertLegend( QwtLegend() )
+
+        self.populate()
+        self.setOrientation( 0 )
+
+        self.setAutoReplot( True )
+
+    def populate(self):
+        colors = ["DarkOrchid", "SteelBlue", "Gold"]
+
+        numSamples = 5
+        numBars = len(colors )
+
+        titles = []
+        for i in range(numBars):
+            titles += "Bar %d"%i
+
+        self.d_barChartItem.setBarTitles( titles )
+        self.d_barChartItem.setLegendIconSize( QSize( 10, 14 ) )
+
+        for i in range(numBars):
+            symbol = Qwt.QwtColumnSymbol( Qwt.QwtColumnSymbol.Box )
+            symbol.setLineWidth( 2 )
+            symbol.setFrameStyle( QwtColumnSymbol.Raised )
+            symbol.setPalette( QPalette( colors[i] ) )
+            self.d_barChartItem.setSymbol( i, symbol )
+        
+        series = np.array([])
+        for i in range(numSamples):
+            values = np.array([])
+            for j in range(numBars):
+                values = np.append(values, ( 2 + random.randint() % 8 ))
+            series = np.append(series,values)
+
+        self.d_barChartItem.setSamples( series )
+
+    def setMode( self, mode ):
+        if ( mode == 0 ):
+            self.d_barChartItem.setStyle( Qwt.QwtPlotMultiBarChart.Grouped )
+        else:
+            self.d_barChartItem.setStyle( Qwt.QwtPlotMultiBarChart.Stacked )
+
+    def setOrientation( self, orientation ):
+        axis1 = Qwt.QwtPlot.Axis()
+        axis2 = Qwt.QwtPlot.Axis()
+        if ( orientation == 0 ):
+            axis1 = Qwt.QwtPlot.xBottom
+            axis2 = Qwt.QwtPlot.yLeft
+            d_barChartItem.setOrientation( Qt.Vertical )
+        else:
+            axis1 = Qwt.QwtPlot.yLeft
+            axis2 = Qwt.QwtPlot.xBottom
+            d_barChartItem.setOrientation( Qt.Horizontal )
+
+        self.setAxisScale( axis1, 0, d_barChartItem.dataSize() - 1, 1.0 )
+        self.setAxisAutoScale( axis2 )
+
+        self.scaleDraw1 = Qwt.QwtScaleDraw.axisScaleDraw( axis1 )
+        self.scaleDraw1.enableComponent( Qwt.QwtScaleDraw.Backbone, False )
+        self.scaleDraw1.enableComponent( Qwt.QwtScaleDraw.Ticks, False )
+
+        self.scaleDraw2 = Qwt.QwtScaleDraw.axisScaleDraw( axis2 )
+        self.scaleDraw2.enableComponent( Qwt.QwtScaleDraw.Backbone, True )
+        self.scaleDraw2.enableComponent( Qwt.QwtScaleDraw.Ticks, True )
+
+        self.plotLayout().setAlignCanvasToScale( axis1, True )
+        self.plotLayout().setAlignCanvasToScale( axis2, False )
+
+        self.plotLayout().setCanvasMargin( 0 )
+        self.updateCanvasMargins()
+        self.replot()
+
+    def exportChart(self):
+        renderer = Qwt.QwtPlotRenderer()
+        renderer.exportTo( self, "barchart.pdf" )
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
+        self.d_chart = BarChart()
+        self.d_chart = BarChart( self )
+        self.setCentralWidget( self.d_chart )
+
+        self.toolBar = QToolBar( self )
+
+        self.typeBox = QComboBox( self.toolBar )
+        self.typeBox.addItem( "Grouped" )
+        self.typeBox.addItem( "Stacked" )
+        self.typeBox.setSizePolicy( QSizePolicy.Fixed, QSizePolicy.Fixed )
+
+        self.orientationBox = QComboBox( self.toolBar )
+        self.orientationBox.addItem( "Vertical" )
+        self.orientationBox.addItem( "Horizontal" )
+        self.orientationBox.setSizePolicy( QSizePolicy.Fixed, QSizePolicy.Fixed )
+
+        self.btnExport = QToolButton( toolBar )
+        self.btnExport.setText( "Export" )
+        self.btnExport.setToolButtonStyle( Qt.ToolButtonTextUnderIcon )
+        self.btnExport.clicked.connect(self.d_chart.exportChart)
+
+        self.toolBar.addWidget( self.typeBox )
+        self.toolBar.addWidget( self.orientationBox )
+        self.toolBar.addWidget( self.btnExport )
+        self.addToolBar( self.toolBar )
+
+        self.d_chart.setMode( typeBox.currentIndex() )
+        self.typeBox.currentIndexChanged['int'].connect(self.d_chart.setMode )
+        self.d_chart.setOrientation( orientationBox.currentIndex() )
+        self.orientationBox.currentIndexChanged['int'].connect(self.d_chart.setOrientation)
+
+a = QApplication(sys.argv)
+m = MainWindow()
+m.resize( 600, 400 )
+m.show()
+sys.exit(a.exec_())
+
