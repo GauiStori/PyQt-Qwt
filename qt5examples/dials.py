c653c8fb99471e93bff577fb7211ca4fd7c5539a
Gudjon I. Gudjonsson <gudjon@fessender>
2018-12-23 11:57:36 +0100

    Update dials.py. Works with a single compass. Crashes when more are added.

--- qt5examples/dials.py
+++ qt5examples/dials.py
@@ -11,25 +11,27 @@ import numpy as np
 from PyQt5.QtCore import pyqtSignal, Qt,  QSize, QBasicTimer
 from PyQt5.QtGui import QColor,  QPixmap, QFont,  QIcon,  QPalette, QLinearGradient
 from PyQt5.QtWidgets import (QMainWindow,  QWidget,  QToolBar,  QToolButton,  QHBoxLayout,  QLabel,  QApplication,  QSizePolicy, 
-    QVBoxLayout,  QFrame, QTabWidget )
+    QVBoxLayout,  QFrame, QTabWidget,  QGridLayout )
 #from PyQt5.QtPrintSupport import QPrintDialog, QPrinter
 
 
 
 class CompassGrid( QWidget ): #QFrame( parent )
-    self.p = QPalette()
-    p.setColor( backgroundRole(), Qt.gray )
-    setPalette( p )
-    setAutoFillBackground( true )
-    layout = QGridLayout( self )
-    layout.setSpacing( 5 )
-    layout.setMargin( 0 )
-    for i in range(6):
-        QwtCompass *compass = createCompass( i )
-        layout.addWidget( compass, i / 3, i % 3 )
+    def __init__(self, parent=None):
+        QWidget.__init__(self,  parent)
+        p = QPalette()
+        p.setColor( self.backgroundRole(), Qt.gray )
+        self.setPalette( p )
+        self.setAutoFillBackground( True )
+        layout = QGridLayout( self )
+        layout.setSpacing( 5 )
+        #layout.setMargin( 0 )
+        for i in range(1):
+            compass = self.createCompass( i )
+            layout.addWidget( compass, i / 3, i % 3 )
 
-    for i in range(layout.columnCount()):
-        layout.setColumnStretch( i, 1 )
+        #for i in range(layout.columnCount()):
+        #    layout.setColumnStretch( i, 1 )
 
     def createCompass( self, pos ):
         palette0 = QPalette()
@@ -37,128 +39,122 @@ class CompassGrid( QWidget ): #QFrame( parent )
             colorRole = QPalette.ColorRole( c )
             palette0.setColor( colorRole, QColor() )
 
-        palette0.setColor( QPalette.Base,
-            palette().color( backgroundRole() ).light( 120 ) )
+        #palette0.setColor( QPalette.Base, palette().color( QPalette.backgroundRole() ).light( 120 ) )
         palette0.setColor( QPalette.WindowText,
             palette0.color( QPalette.Base ) )
 
-        compass = QwtCompass( self )
+        compass = Qwt.QwtCompass( self )
         compass.setLineWidth( 4 )
-        compass.setFrameShadow(
-            pos <= 2 ? QwtCompass.Sunken : QwtCompass.Raised )
+        #compass.setFrameShadow(
+        #    pos <= 2 ? QwtCompass.Sunken : QwtCompass.Raised )
 
         if pos == 0:
             #A compass with a rose and no needle. Scale and rose are rotating.
-            compass.setMode( QwtCompass.RotateScale )
+            compass.setMode( Qwt.QwtCompass.RotateScale )
 
-            QwtSimpleCompassRose *rose = new QwtSimpleCompassRose( 16, 2 )
+            rose = Qwt.QwtSimpleCompassRose( 16, 2 )
             rose.setWidth( 0.15 )
 
             compass.setRose( rose )
         elif pos == 1:
             #A windrose, with a scale indicating the main directions only
-            QMap<double, QString> map
-            map.insert( 0.0, "N" )
-            map.insert( 90.0, "E" )
-            map.insert( 180.0, "S" )
-            map.insert( 270.0, "W" )
+            map = {}
+            map[0] = "N" 
+            map[90] = "E" 
+            map[180] = "S" 
+            map[270] = "W" 
 
-            compass.setScaleDraw( new QwtCompassScaleDraw( map ) )
+            compass.setScaleDraw( Qwt.QwtCompassScaleDraw( map ) )
 
-            QwtSimpleCompassRose *rose = new QwtSimpleCompassRose( 4, 1 )
+            rose = Qwt.QwtSimpleCompassRose( 4, 1 )
             compass.setRose( rose )
 
-            compass.setNeedle(
-                new QwtCompassWindArrow( QwtCompassWindArrow.Style2 ) )
-            compass.setValue( 60.0 )
+            #compass.setNeedle( Qwt.QwtCompassWindArrow( Qwt.QwtCompassWindArrow.Style2 ) )
+            #compass.setValue( 60.0 )
         elif pos == 2:
             #A compass with a rotating needle in darkBlue. Shows
             #a ticks for each degree.
 
             palette0.setColor( QPalette.Base, Qt.darkBlue )
-            palette0.setColor( QPalette.WindowText,
-                            QColor( Qt.darkBlue ).dark( 120 ) )
+            #palette0.setColor( QPalette.WindowText, QColor( Qt.darkBlue ).dark( 120 ) )
             palette0.setColor( QPalette.Text, Qt.white )
 
-            QwtCompassScaleDraw *scaleDraw = new QwtCompassScaleDraw()
-            scaleDraw.enableComponent( QwtAbstractScaleDraw.Ticks, true )
-            scaleDraw.enableComponent( QwtAbstractScaleDraw.Labels, true )
-            scaleDraw.enableComponent( QwtAbstractScaleDraw.Backbone, false )
-            scaleDraw.setTickLength( QwtScaleDiv.MinorTick, 1 )
-            scaleDraw.setTickLength( QwtScaleDiv.MediumTick, 1 )
-            scaleDraw.setTickLength( QwtScaleDiv.MajorTick, 3 )
+            scaleDraw = Qwt.QwtCompassScaleDraw()
+            scaleDraw.enableComponent( Qwt.QwtAbstractScaleDraw.Ticks, True )
+            scaleDraw.enableComponent( Qwt.QwtAbstractScaleDraw.Labels, True )
+            scaleDraw.enableComponent( Qwt.QwtAbstractScaleDraw.Backbone, False )
+            scaleDraw.setTickLength( Qwt.QwtScaleDiv.MinorTick, 1 )
+            scaleDraw.setTickLength( Qwt.QwtScaleDiv.MediumTick, 1 )
+            scaleDraw.setTickLength( Qwt.QwtScaleDiv.MajorTick, 3 )
 
             compass.setScaleDraw( scaleDraw )
 
             compass.setScaleMaxMajor( 36 )
             compass.setScaleMaxMinor( 5 )
 
-            compass.setNeedle(
-                new QwtCompassMagnetNeedle( QwtCompassMagnetNeedle.ThinStyle ) )
+            compass.setNeedle( Qwt.QwtCompassMagnetNeedle( Qwt.QwtCompassMagnetNeedle.ThinStyle ) )
             compass.setValue( 220.0 )
         elif pos == 3:
             #A compass without a frame, showing numbers as tick labels.
             #The origin is at 220.0
-            palette0.setColor( QPalette.Base, palette().color( backgroundRole() ) )
+            #palette0.setColor( QPalette.Base, palette().color( backgroundRole() ) )
             palette0.setColor( QPalette.WindowText, Qt.blue )
             compass.setLineWidth( 0 )
-            QMap<double, QString> map
+            map = {}
             for  d in range(0,360,60):
-                map.insert( d, "%.0f"%(d*1.0) )
+                map[d] = "%.0f"%(d*1.0) 
 
-            QwtCompassScaleDraw *scaleDraw = new QwtCompassScaleDraw( map )
-            scaleDraw.enableComponent( QwtAbstractScaleDraw.Ticks, true )
-            scaleDraw.enableComponent( QwtAbstractScaleDraw.Labels, true )
-            scaleDraw.enableComponent( QwtAbstractScaleDraw.Backbone, true )
-            scaleDraw.setTickLength( QwtScaleDiv.MinorTick, 0 )
-            scaleDraw.setTickLength( QwtScaleDiv.MediumTick, 0 )
-            scaleDraw.setTickLength( QwtScaleDiv.MajorTick, 3 )
+            scaleDraw = Qwt.QwtCompassScaleDraw( map )
+            scaleDraw.enableComponent( Qwt.QwtAbstractScaleDraw.Ticks, True )
+            scaleDraw.enableComponent( Qwt.QwtAbstractScaleDraw.Labels, True )
+            scaleDraw.enableComponent( Qwt.QwtAbstractScaleDraw.Backbone, True )
+            scaleDraw.setTickLength( Qwt.QwtScaleDiv.MinorTick, 0 )
+            scaleDraw.setTickLength( Qwt.QwtScaleDiv.MediumTick, 0 )
+            scaleDraw.setTickLength( Qwt.QwtScaleDiv.MajorTick, 3 )
             compass.setScaleDraw( scaleDraw )
             compass.setScaleMaxMajor( 36 )
             compass.setScaleMaxMinor( 5 )
-            compass.setNeedle( new QwtDialSimpleNeedle( QwtDialSimpleNeedle.Ray,
-                true, Qt.white ) )
+            compass.setNeedle( Qwt.QwtDialSimpleNeedle( Qwt.QwtDialSimpleNeedle.Ray, True, Qt.white ) )
             compass.setOrigin( 220.0 )
             compass.setValue( 20.0 )
         elif pos == 4:
             #A compass showing another needle
-            QwtCompassScaleDraw *scaleDraw = new QwtCompassScaleDraw()
-            scaleDraw.enableComponent( QwtAbstractScaleDraw.Ticks, true )
-            scaleDraw.enableComponent( QwtAbstractScaleDraw.Labels, true )
-            scaleDraw.enableComponent( QwtAbstractScaleDraw.Backbone, false )
-            scaleDraw.setTickLength( QwtScaleDiv.MinorTick, 0 )
-            scaleDraw.setTickLength( QwtScaleDiv.MediumTick, 0 )
-            scaleDraw.setTickLength( QwtScaleDiv.MajorTick, 3 )
+            scaleDraw = Qwt.QwtCompassScaleDraw()
+            scaleDraw.enableComponent( Qwt.QwtAbstractScaleDraw.Ticks, True )
+            scaleDraw.enableComponent( Qwt.QwtAbstractScaleDraw.Labels, True )
+            scaleDraw.enableComponent( Qwt.QwtAbstractScaleDraw.Backbone, False )
+            scaleDraw.setTickLength( Qwt.QwtScaleDiv.MinorTick, 0 )
+            scaleDraw.setTickLength( Qwt.QwtScaleDiv.MediumTick, 0 )
+            scaleDraw.setTickLength( Qwt.QwtScaleDiv.MajorTick, 3 )
 
             compass.setScaleDraw( scaleDraw )
 
-            compass.setNeedle( new QwtCompassMagnetNeedle(
-                QwtCompassMagnetNeedle.TriangleStyle, Qt.white, Qt.red ) )
+            compass.setNeedle( Qwt.QwtCompassMagnetNeedle(
+                Qwt.QwtCompassMagnetNeedle.TriangleStyle, Qt.white, Qt.red ) )
             compass.setValue( 220.0 )
         elif pos == 5:
             #A compass with a yellow on black ray
             palette0.setColor( QPalette.WindowText, Qt.black )
-            compass.setNeedle( new QwtDialSimpleNeedle( QwtDialSimpleNeedle.Ray,
-                false, Qt.yellow ) )
+            #compass.setNeedle( Qwt.QwtDialSimpleNeedle( Qwt.QwtDialSimpleNeedle.Ray, False, Qt.yellow ) )
             compass.setValue( 315.0 )
 
-        QPalette newPalette = compass.palette()
-        for c in range(QPalette.NColorRoles)
+        newPalette = compass.palette()
+        for c in range(QPalette.NColorRoles):
             colorRole = QPalette.ColorRole( c )
             if ( palette0.color( colorRole ).isValid() ):
                 newPalette.setColor( colorRole, palette0.color( colorRole ) )
 
-        for i in range(QPalette.NColorGroups):
-            colorGroup = QPalette.ColorGroup( i )
-            light = newPalette.color( colorGroup, QPalette.Base ).light( 170 )
-            dark = newPalette.color( colorGroup, QPalette.Base ).dark( 170 )
-            mid = compass.frameShadow() == QwtDial.Raised
-                ? newPalette.color( colorGroup, QPalette.Base ).dark( 110 )
-                : newPalette.color( colorGroup, QPalette.Base ).light( 110 )
-
-            newPalette.setColor( colorGroup, QPalette.Dark, dark )
-            newPalette.setColor( colorGroup, QPalette.Mid, mid )
-            newPalette.setColor( colorGroup, QPalette.Light, light )
+        #for i in range(QPalette.NColorGroups):
+        #    #colorGroup = QPalette.ColorGroup( i )
+        #    #light = newPalette.color( colorGroup, QPalette.Base ).light( 170 )
+        #    #dark = newPalette.color( colorGroup, QPalette.Base ).dark( 170 )
+        #    #mid = compass.frameShadow() == QwtDial.Raised
+        #    #    ? newPalette.color( colorGroup, QPalette.Base ).dark( 110 )
+        #    #    : newPalette.color( colorGroup, QPalette.Base ).light( 110 )
+        #    mid = newPalette.color( colorGroup, QPalette.Base ).dark( 110 )
+        #    newPalette.setColor( colorGroup, QPalette.Dark, dark )
+        #    newPalette.setColor( colorGroup, QPalette.Mid, mid )
+        #    newPalette.setColor( colorGroup, QPalette.Light, light )
         compass.setPalette( newPalette )
         return compass
 
@@ -168,7 +164,7 @@ class CompassGrid( QWidget ): #QFrame( parent )
 
 a = QApplication(sys.argv)
 tabWidget = QTabWidget()
-#tabWidget.addTab(CompassGrid,"Compass")
+tabWidget.addTab(CompassGrid(),"Compass")
 #tabWidget.addTab("Cockpit")
 tabWidget.show()
 
